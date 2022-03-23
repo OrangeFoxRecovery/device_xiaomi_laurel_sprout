09acf13aa7095947c4a3fdb3288c43a13cfe3402
Sushrut1101 <guptasushrut@gmail.com>
2022-03-23 16:53:49 +0530

    mojito: Use QCOM twrp-common trees for Decryption
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>
    [RealAkira : Updated  device qcom twrp common to andriod 12.1]
    
    Signed-off-by: RealAkira <talukdarrahul32@gmail.com>

--- device.mk
+++ device.mk
@@ -44,4 +44,10 @@ PRODUCT_PACKAGES_DEBUG += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/qcom-caf/bootctrl \
-    vendor/qcom/opensource/commonsys-intf/display
+    vendor/qcom/opensource/commonsys-intf/display \
+    hardware/qcom-caf/bootctrl
+
+# QCOM Decryption
+PRODUCT_PACKAGES += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
